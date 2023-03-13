7a6605d337e795a2dad839000f0443dd4875fe7e
Mélissande Dizy <78364535+lestox@users.noreply.github.com>
2023-03-13 15:30:10 +0100

    Début endpoint

--- back/main.py
+++ back/main.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/login")
+async def login(username: str, password: str):
+    if username != "toto":
+        raise HTTPException(status_code=404, detail="User not found")
+    return {"Successfully login"}
+
+
+@app.post("/register")
+async def register():
     return {"message": "Hello World"}
