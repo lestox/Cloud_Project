e8cd4e88644bf775373a8df4b75cc0b6063590a7
Doustalet Chloé <cdoustalet@gmail.com>
2023-03-14 17:01:53 +0100

    first step to create Menu Organism

---
+++ front/src/components/UI/molecules/Menu.svelte
@@ -0,0 +1,61 @@
+<script>
+  import Link from "../atoms/Link.svelte";
+  import Icon from "../atoms/Icon.svelte";  
+  
+  import HomeIcon from "../../../assets/icons/HomeIcon.svelte";
+  import AccountIcon from "../../../assets/icons/AccountIcon.svelte";
+  import LogoutIcon from "../../../assets/icons/LogoutIcon.svelte";
+
+</script>
+
+<div class="menu">
+  <div class="menu-el menu-el--active">
+    <Icon name="home" >
+      <HomeIcon />
+    </Icon>
+    <Link
+    linkUrl='/reset-password'> 
+      My cloud
+    </Link>
+  </div>
+
+  <div class="menu-el">
+    <Icon name="account">
+      <AccountIcon />
+    </Icon>
+    <Link
+    linkUrl='/reset-password'> 
+      Account 
+    </Link>
+  </div>
+
+  <div class="menu-el">
+    <Icon name="logout">
+      <LogoutIcon />
+    </Icon>
+    <Link
+    linkUrl='/reset-password'> 
+      Logout
+    </Link>
+  </div>
+</div>
+
+<style>
+  .menu{
+    width: 225px;
+    margin-top: 85px;
+  }
+
+  .menu-el{
+    display: flex;
+    align-items: center;
+    column-gap: 10px;
+    margin: 10px 0;
+    padding: 10px 15px;
+  }
+
+  .menu-el--active{
+    background-color: var(--color-background-secondary);
+    border-radius: var(--x-small-radius);
+  }
+</style>
---
+++ front/src/components/UI/organisms/SidebarMenu.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Menu from "../molecules/Menu.svelte";
+  import Image from "../atoms/Image.svelte";
+</script>
+
+<div class="sidebar-menu">
+  <Image
+    imageSrc="./../assets/img/logo_color.png"
+    imageAlt="SkyCloud Logo"
+  />
+  <Menu />
+</div>
+
+<style>
+  .sidebar-menu{
+    width: 10%;
+    min-width: 300px;
+    height: 100vh;  
+    padding-left: 40px; 
+    padding-top: 50px;
+
+    background-color: aquamarine;
+  }
+
+  .sidebar-menu img{
+    width: 225px;
+    height: auto;
+    object-fit: cover;
+  }
+</style>
