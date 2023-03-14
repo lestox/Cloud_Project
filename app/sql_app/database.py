5ecd913241627d171f2e5a0e946af8b0edc27647
Mélissande Dizy <78364535+lestox@users.noreply.github.com>
2023-03-14 20:11:49 +0100

    resolution d'1 erreru

--- app/sql_app/database.py
+++ app/sql_app/database.py
@@ -1,13 +1,14 @@
 from sqlalchemy import create_engine
+import pymysql
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Define the MariaDB engine using MariaDB Connector/Python
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:password@127.0.0.1:8081/data"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:password@127.0.0.1:3306/data"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
