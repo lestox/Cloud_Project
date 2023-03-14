8896ffe76d6d99aef1748a783bf6b6a598dddcd5
ValentinR01 <valentin.rejaunier@hetic.net>
2023-03-15 00:44:30 +0100

    Fix input border on focus

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,5 +1,12 @@
 <script>
   import '../assets/global.css';
+  import Icon from '../components/UI/atoms/Icon.svelte';
+  import ProjectIcon from '../assets/img/logo_white.png';
+  import Image from '../components/UI/atoms/Image.svelte';
 </script>
+
+<Image src="../assets/img/logo_white.png"
+alt="Hello"
+class="image--large" />
 
 
