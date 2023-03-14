06add5e50e99b228f90b917b13a93ebe0e5495c7
Doustalet Chloé <cdoustalet@gmail.com>
2023-03-14 15:42:16 +0100

    add Icon Component and Images

---
+++ front/src/components/UI/atoms/Image.svelte
@@ -0,0 +1,39 @@
+<script>
+  /**
+   * @type {string}
+  */
+  export let imageSrc = '';
+  export let imageAlt = 'image';
+  export let imageStyle = 'image--large';
+</script>
+
+<img
+  src={imageSrc}
+  alt={imageAlt}
+  class={imageStyle}
+/>
+
+<style>
+  .image--large{
+    height: 775px;
+    width: 895px;
+    object-fit: cover;
+    border-radius: var(--border-radius);
+  }
+
+  .icon--small{
+    height: 20px;
+    width: auto;
+  }
+
+  .icon-medium{
+    height: 35px;
+    width: 35px;
+    object-fit: cover;
+  }
+
+  .icon--large{
+    height: 70px;
+    width: auto;
+  }
+</style>
--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,9 +1,21 @@
 <script>
+  import '../assets/global.css';
+
   import Text from '../components/UI/atoms/Text.svelte';
   import Button from '../components/UI/atoms/Button.svelte';
   import Link from '../components/UI/atoms/Link.svelte';
   import Form from '../components/UI/molecules/Form.svelte';
-  import '../assets/global.css';
+  import Image from '../components/UI/atoms/Image.svelte';
+  import Icon from '../components/UI/atoms/Icon.svelte';
+
+  import HomeIcon from '../assets/icons/HomeIcon.svelte';
+  import AccountIcon from '../assets/icons/AccountIcon.svelte';
+  import LogoutIcon from '../assets/icons/LogoutIcon.svelte';
+  import UserIcon from '../assets/icons/UserIcon.svelte';
+  import DatabaseIcon from '../assets/icons/DatabaseIcon.svelte';
+  import StorageIcon from '../assets/icons/StorageIcon.svelte';
+  import BackupIcon from '../assets/icons/BackupIcon.svelte';
+  import ProjectIcon from '../assets/icons/ProjectIcon.svelte';
 </script>
 
 <Text 
@@ -23,3 +35,28 @@ buttonWidth='button--full'>
 linkUrl='/reset-password'> 
   Mot de passe oublié ? 
 </Link>
+
+<Icon name="home" >
+  <HomeIcon />
+</Icon>
+<Icon name="account">
+  <AccountIcon />
+</Icon>
+<Icon name="logout">
+  <LogoutIcon />
+</Icon>
+<Icon name="user" iconClass="icon--large">
+  <UserIcon />
+</Icon>
+<Icon name="project" iconClass="icon-project">
+  <ProjectIcon />
+</Icon>
+<Icon name="database" iconClass="icon--medium">
+  <DatabaseIcon />
+</Icon>
+<Icon name="storage" iconClass="icon--medium">
+  <StorageIcon />
+</Icon>
+<Icon name="backup" iconClass="icon--medium">
+  <BackupIcon />
+</Icon>
