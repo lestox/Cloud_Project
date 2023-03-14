502b79511e2fa80cb0b24562816600781666ce45
Mélissande Dizy <78364535+lestox@users.noreply.github.com>
2023-03-14 11:57:06 +0100

    ajout dump.sql

--- db/sql/site.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS site (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    name VARCHAR(255),
-    url VARCHAR(255),
-    description TEXT,
-    FOREIGN KEY (user_id) REFERENCES user(id)
-);
