2b2ccbfdbe3f38797d38cbc17a18cf5d5f3d7b8e
DFarau <doriane.farau@hetic.net>
2023-03-14 15:34:41 +0100

    connection with database

--- app/main.py
+++ app/main.py
@@ -1,20 +1,42 @@
-from fastapi import FastAPI, HTTPException
+from typing import List
+
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from .sql_app import crud, models, schemas
+from .sql_app.database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
-@app.post("/login")
-async def login(username: str, password: str):
-    if username != "toto":
+@app.post("/login", status_code=200)
+def login(email: str, password: str, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
+    encrypted_password = password + "notreallyhashed"
+    if db_user["password"] != encrypted_password:
+        raise HTTPException(status_code=401, detail="Invalid Password")
     return {"Successfully login"}
 
 
-@app.post("/register")
-async def register():
-    return {"message": "Hello World"}
+@app.post("/register", status_code=200)
+def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
