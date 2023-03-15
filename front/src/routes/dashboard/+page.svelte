a251dc2a38cc81c20a98144a224f15867970e22a
Doustalet Chloé <cdoustalet@gmail.com>
2023-03-15 15:57:32 +0100

    Sidebar Infos created

--- front/src/routes/dashboard/+page.svelte
+++ front/src/routes/dashboard/+page.svelte
@@ -1,11 +1,21 @@
 <script>
   import '../../assets/global.css';
   import '../../assets/css/UserCard.css'
+  import '../../assets/css/InfoCard.css'
   
   import SidebarMenu from "../../components/UI/organisms/SidebarMenu.svelte";
-  import UserCard from '../../components/UI/molecules/UserCard.svelte';
+  import SidebarInfos from '../../components/UI/organisms/SidebarInfos.svelte';
 </script>
 
-<SidebarMenu />
-<h1>Dashboard Page</h1>
-<UserCard />
+<div class='page'>
+  <SidebarMenu />
+  <h1>Dashboard Page</h1>
+  <SidebarInfos />
+</div>
+
+<style>
+  .page{
+    background-color: var(--color-background-secondary);
+    display: flex;
+  }  
+</style>
