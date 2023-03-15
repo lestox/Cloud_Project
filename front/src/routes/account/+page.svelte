d63628b59da08214f9294745359030c5a91f1a56
Doustalet Chloé <cdoustalet@gmail.com>
2023-03-15 11:43:10 +0100

    Menu Fix

--- front/src/routes/account/+page.svelte
+++ front/src/routes/account/+page.svelte
@@ -1,2 +1,9 @@
+<script>
+  import '../../assets/global.css';
+  import SidebarMenu from "../../components/UI/organisms/SidebarMenu.svelte";
+</script>
+ 
+<SidebarMenu current='account'/>
 <h1>Profil infos</h1>
+
 
