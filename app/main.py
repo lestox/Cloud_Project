285f72bb75b051a6e09c82b5ccf13aaffa9a1155
TaniaMAHOUCHE <mahouchetania15@gmail.com>
2023-03-16 14:17:53 +0100

    Added jwt

--- app/main.py
+++ app/main.py
@@ -9,10 +9,13 @@ import logging
 from app.sql_app.db import get_session
 from app.sql_app.models import Users, UsersCreate
 from app.sql_app.user_password import Hasher
+from app.jwt.auth import AuthHandler
 
 app = FastAPI()
 
 logger = logging.getLogger(__name__)
+
+auth_handler = AuthHandler()
 
 
 @app.get("/ping")
@@ -27,14 +30,18 @@ async def get_users(session: AsyncSession = Depends(get_session)):
     return 200, users
 
 
-@app.post("/register")
+@app.post("/register", status_code=201)
 async def register(users: UsersCreate, session: AsyncSession = Depends(get_session)):
+    query_email = await session.execute(select(Users).where(Users.email == users.email))
+    search_email = query_email.scalars().all()
+    if search_email:
+        raise HTTPException(status_code=409, detail="User already exist")
     encrypted_password = Hasher.get_password_hash(users.password)
     user = Users(fullname=users.fullname, email=users.email, password=encrypted_password)
     session.add(user)
     await session.commit()
     await session.refresh(user)
-    return 201, user
+    return user
 
 
 @app.get("/login")
@@ -44,12 +51,20 @@ async def login(email, password, session: AsyncSession = Depends(get_session)):
     if not search_email:
         raise HTTPException(status_code=404, detail="User not found")
 
-    query_password = await session.execute(select(Users).where(Users.email == email))
-    search_password = query_password.scalars().all()
-    user_password = search_password[0].password
-
+    user_password = search_email[0].password
     verify_password = Hasher.verify_password(plain_password=password, hashed_password=user_password)
     if verify_password:
-        return 200, "Successfully login"
+        token = auth_handler.encode_token(email)
+        return f'Token : {token} . Successfully login'
     else:
         raise HTTPException(status_code=401, detail="Wrong password")
+
+
+@app.get('/unprotected')
+def unprotected():
+    return {'hello': 'world'}
+
+
+@app.get('/protected')
+def protected(username=Depends(auth_handler.auth_wrapper)):
+    return {'name': username}
